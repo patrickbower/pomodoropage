7191987c7f9154281200d166b0387ed165161f52
Patrick Bower <patrickbower@Patricks-MacBook-Pro-2.local>
2017-10-24 23:56:46 +0100

    Begin setup for Firebase

---
+++ public/test.js
@@ -0,0 +1,14 @@
+// console.log("hello world");
+// // Initialize Firebase
+// var config = {
+//   apiKey: "AIzaSyAjm0pNnzZ1Cr9-ur2IqbvWzUZ-x9nOS2g",
+//   authDomain: "pomodoropage.firebaseapp.com",
+//   databaseURL: "https://pomodoropage.firebaseio.com",
+//   projectId: "pomodoropage",
+//   storageBucket: "",
+//   messagingSenderId: "674003386548"
+// };
+// // firebase.initializeApp(config);
+//
+// var database = firebase.database();
+// console.log("database", database);
